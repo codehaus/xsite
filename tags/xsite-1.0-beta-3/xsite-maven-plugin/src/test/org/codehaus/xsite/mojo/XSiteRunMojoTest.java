b9aa6553862504d38c289df4f42bda1f3fc07a6b
mauro <mauro@cd184976-2b07-0410-8b60-c68cbc61095d>
2006-10-26 15:00:34 +0000

    [maven-scm] copy for tag xsite-1.0-beta-3

---
+++ tags/xsite-1.0-beta-3/xsite-maven-plugin/src/test/org/codehaus/xsite/mojo/XSiteRunMojoTest.java
@@ -0,0 +1,64 @@
+package org.codehaus.xsite.mojo;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+public class XSiteRunMojoTest  extends TestCase {
+    protected  String testSrcDir;
+    
+    public void setUp() throws Exception {
+        setTestDir();
+    }    
+    
+    protected void setTestDir() {
+        testSrcDir =  System.getProperty("test.src.dir");
+        if ( testSrcDir == null ){
+            testSrcDir = "xsite-maven-plugin/src/test/"; 
+        } else if ( !testSrcDir.endsWith(File.separator) ){
+            testSrcDir = testSrcDir + File.separator; 
+        }        
+    }
+    
+    public void testGoalCanBeRunWithDefaultComposition() throws Exception {
+        XSiteRunMojo mojo = new XSiteRunMojo();
+        mojo.siteMapPath = testSrcDir+"content/website.xml";
+        mojo.skinPath = testSrcDir+"templates/skin.html";
+        mojo.outputDirectoryPath = "target/xsite";
+        mojo.execute();
+    }
+
+    public void testGoalCanBeRunWithOptionalFileComposition() throws Exception {
+        XSiteRunMojo mojo = new XSiteRunMojo();
+        mojo.siteMapPath = testSrcDir+"content/website.xml";
+        mojo.skinPath = testSrcDir+"templates/skin.html";
+        mojo.outputDirectoryPath = "target/xsite";
+        mojo.compositionFilePath = testSrcDir+"templates/xsite.xml";
+        mojo.execute();
+    }
+
+    public void testGoalCanBeRunWithOptionalResourceComposition() throws Exception {
+        XSiteRunMojo mojo = new XSiteRunMojo();
+        mojo.siteMapPath = testSrcDir+"content/website.xml";
+        mojo.skinPath = testSrcDir+"templates/skin.html";
+        mojo.outputDirectoryPath = "target/xsite";
+        mojo.compositionResourcePath = "templates/xsite.xml";
+        mojo.execute();
+    }
+    
+    public void testGoalFailsWithInvalidPath() throws Exception {
+        XSiteRunMojo mojo = new XSiteRunMojo();
+        mojo.siteMapPath = testSrcDir+"content/non-existent.xml";
+        mojo.skinPath = testSrcDir+"templates/skin.html";
+        mojo.outputDirectoryPath = "target/xsite";
+        try {
+	    mojo.execute();
+	    fail("MojoExecutionException expected");
+	} catch ( MojoExecutionException e) {
+	    // expected
+	}
+    }
+}
+
